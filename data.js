0834649702dbbea637a9cf1be1aa73b771f3e7fe
Kaique Matheus <Kaique_KMCR@outlook.com>
2022-01-22 12:31:17 -0300

    add files

---
+++ data.js
@@ -0,0 +1,36 @@
+let songs = [
+  {
+    name: 'Ghost',
+    path: './assets/audio/Ghost.mp3',
+    artist: 'KMCR',
+    cover: 'assets/cover/albumPicture.png'
+  },
+
+  {
+    name: 'Kyoto [Prototype]',
+    path: './assets/audio/Kyoto [Prototype].mp3',
+    artist: 'KMCR',
+    cover: 'assets/cover/albumPicture.png'
+  },
+
+  {
+    name: 'Mysterio [Prototype]',
+    path: 'assets/audio/Mysterio [Prototype].mp3',
+    artist: 'KMCR',
+    cover: 'assets/cover/albumPicture.png'
+  },
+
+  {
+    name: 'Oriental',
+    path: 'assets/audio/Oriental.mp3',
+    artist: 'KMCR',
+    cover: 'assets/cover/albumPicture.png'
+  },
+
+  {
+    name: 'Suburban [Prototype]',
+    path: 'assets/audio/Suburban [Prototype].mp3',
+    artist: 'KMCR',
+    cover: 'assets/cover/albumPicture.png'
+  }
+]
